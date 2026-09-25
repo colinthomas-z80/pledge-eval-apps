49ad08fa06df2ab96eba6a6d0819cb95efb1b4ca
Colin Thomas <cthomas0687@gmail.com>
2026-09-25 16:31:42 -0400

    update star script

--- star-gatk/star_gatk.sh
+++ star-gatk/star_gatk.sh
@@ -2,12 +2,13 @@
 #set -e 
 set -o pipefail
 
-FastqFiles=("SRR10307460" "ERR4179434" "SRR40644330" "SRR40639328" "SRR40639326" "SRR40315802" "SRR40315804" "SRR40315804" "SRR40315805" "SRR40315806")
+#FastqFiles=("SRR10307460" "ERR4179434" "SRR40644330" "SRR40639328" "SRR40639326" "SRR40315802" "SRR40315804" "SRR40315804" "SRR40315805" "SRR40315806")
+FastqFiles=("SRR10307460")
 FastqBase="${FastqFiles[0]}"
 BaseFastaGz="GRCh38.primary_assembly.genome.fa.gz"
 BaseFasta="GRCh38.primary_assembly.genome.fa"
 FastaPrefix="GRCh38.gencode.pa.genome"
-MAXFA=12
+MAXFA=1
 GenomeDir="gdir"
 STAR_filein="${FastqBase}.fastq"
 #head -n 26091 SRR40671052.fastq > srr_trimmed.fastq
@@ -41,7 +42,7 @@ if [ "$1" = "fetch" ]; then
         fi
         j=$((i + 1))
         echo "Extracting chr${i} from Fasta file (${BaseFastaGz})" 
-        sed -n "/>chr${i} ${i}/,+10000p" ${BaseFasta} > ${FastaPrefix}.${i}.fa
+        sed -n "/>chr${i} ${i}/,+10p" ${BaseFasta} > ${FastaPrefix}.${i}.fa
     done
 
 elif [[ $1 = "clean" ]]; then
@@ -83,7 +84,7 @@ else
 	printf -v row "%$(tput cols)s"; echo "${row// /=}"
 	echo "Generate GenomeRequirements.txt"
 	printf -v row "%$(tput cols)s"; echo "${row// /=}"
-	STAR--runThreadN ${ThreadNum} --runMode genomeGenerate --genomeDir ${GenomeDir} --genomeFastaFiles "${FastaPrefix}"*.fa
+	STAR --runThreadN ${ThreadNum} --runMode genomeGenerate --genomeDir ${GenomeDir} --genomeFastaFiles "${FastaPrefix}"*.fa --genomeSAindexNbases 2
 
 	printf -v row "%$(tput cols)s"; echo "${row// /=}"
 	echo "Mapping Fastq files"
